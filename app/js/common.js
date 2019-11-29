2a84a98d45661e66686ba0d7775385cc32b30230
Anton Beren Ivanov <anton@svoj5.ru>
2019-11-29 14:07:45 +0400

    init git on project

---
+++ app/js/common.js
@@ -0,0 +1,85 @@
+$(function() {
+
+	// // Custom JS
+	var map;
+
+            DG.then(function () {
+
+                if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                    // код для мобильных устройств
+                    map = DG.map('map', {
+                        center: [42.871877, 74.586491],
+                        zoom: 13,
+                        dragging: false,
+                        poi: false
+                        // scrollWheelZoom: false
+                    });
+                  } else {
+                    // код для обычных устройств
+                    map = DG.map('map', {
+                        center: [42.871877, 74.586491],
+                        zoom: 13,
+                        poi: false
+                        // dragging: false,
+                        // scrollWheelZoom: false
+                    });
+                    
+                }
+                
+                
+
+				mapicon = DG.icon({
+					iconUrl: '../img/marker.svg', /* Иконка маркера */
+					iconAnchor: [42.871877, 74.586491], 
+					popupAnchor: [42.871877, 74.586491],
+					className: 'map-icon',
+					iconSize: [64, 64] /* Размер иконки */
+				});          
+				DG.marker([42.871877, 74.586491], {icon: mapicon}).addTo(map).bindPopup('<div class="map-popup"><h2>FamsTour</h2><br/>Авто и мото туры<br/>по лесным и горным маршрутам</div>');     /* Координаты маркера и текст попапа */
+            });
+
+
+});
+
+
+(function($) {
+
+    $('[data-fancybox="images"]').fancybox({
+        buttons : [ 
+            'zoom',
+            'slideShow',
+            // 'share',
+            // 'fullScreen',
+            // 'download',
+            "thumbs",
+            'close'
+        ],
+        loop: true,
+        gutter : 10,
+        keyboard: true,
+        arrows: true,
+        infobar: true,
+        smallBtn: "auto",
+        toolbar: true,
+        protect: true,
+        modal: false,
+        idleTime: 3,
+        animationEffect: "zoom",
+        animationDuration: 600,
+        transitionEffect: "fade",
+        transitionDuration: 400,
+        slideClass: "myClass",
+        baseClass: "myclass",
+        slideShow: {
+            autoStart: false,
+            speed: 1000
+        },
+        youtube : {
+        controls : 0,
+        showinfo : 0
+        },
+        thumbs : {
+            autoStart : false
+        }
+        });
+})(jQuery_3_4_1);
